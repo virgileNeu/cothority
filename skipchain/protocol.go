c36c07278a5b3e58f1386c0518ad2e37e5d18355
kc1212 <kc1212@users.noreply.github.com>
2018-01-18 13:09:11 +0100

    Increase extend roster timeout

--- skipchain/protocol.go
+++ skipchain/protocol.go
@@ -175,7 +175,7 @@ func (p *ExtendRoster) HandleExtendRosterReply(r ProtoStructExtendRosterReply) e
 			select {
 			case <-p.doneChan:
 				return
-			case <-time.After(time.Second):
+			case <-time.After(time.Second * 10):
 				p.tempSigsMutex.Lock()
 				defer p.tempSigsMutex.Unlock()
 
